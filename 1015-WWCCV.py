6203db0c44315d3d682482e59174af8ffa6d6433
Osifo <TaronOsifo@my.unt.edu>
2024-10-03 09:15:58 -0500

    Taron Osifo implemented luhn and testing

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -17,7 +17,7 @@ print ("[Your Name's] Credit Card Validator")
 # Let's get the card number from the user
 
 card_number = input("Enter your 16-digit credit card number: ")
-
+#yes
 # Validate the credit card number using the Luhn algorithm
 
 if is_credit_card_valid(card_number):           # we are calling the function above and sending it the card_number to validate
